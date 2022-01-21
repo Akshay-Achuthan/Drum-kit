72f38a24e4171f38163e492952237d31eae1a845
Akshay Achuthan <ithub97@gmail.com>
2022-01-21 18:18:01 +0530

    added case insensitive

--- index.js
+++ index.js
@@ -21,36 +21,43 @@ document.addEventListener("keydown", function (event) {
 
 function soundcheck(key) {
   switch (key) {
+    case "W":
     case "w":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
+    case "A":
     case "a":
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
 
+    case "S":
     case "s":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
+    case "D":
     case "d":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
+    case "J":
     case "j":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
+    case "K":
     case "k":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
+    case "L":
     case "l":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
